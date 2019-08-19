086b2bebea7a35b5446626887a63b98b9e739408
Robert Helgesson <robert@rycee.net>
2019-08-19 12:06:41 +0200

    Pin Nixpkgs in `default.nix`

--- default.nix
+++ default.nix
@@ -1,4 +1,14 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+
+  # Pinned Nixpkgs to known working commit.
+  nixpkgs = builtins.fetchTarball {
+   url = "https://github.com/NixOS/nixpkgs/archive/bc8bc2c7cf3066f8a4e0c365651f968195338422.tar.gz";
+   sha256 = "130y0yqh1snkadgwdgf5hbdhwh1sz9qw5qi8ji9z3n271f779551";
+  };
+
+in
+
+{ pkgs ? import nixpkgs {} }:
 
 pkgs.haskellPackages.developPackage {
   root = ./.;
