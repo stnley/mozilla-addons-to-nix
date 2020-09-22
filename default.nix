48b7598356615ed0b11b3ef8647facee46cef804
Robert Helgesson <robert@rycee.net>
2020-09-22 23:34:14 +0200

    Update Nixpkgs pin

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 let
 
-  # Pinned Nixpkgs to known working commit. Pinned 2020-06-20.
+  # Pinned Nixpkgs to known working commit. Pinned 2020-09-11.
   nixpkgs = builtins.fetchTarball {
     url =
-      "https://github.com/NixOS/nixpkgs/archive/a84cbb60f0296210be03c08d243670dd18a3f6eb.tar.gz";
-    sha256 = "04j07c98iy66hpzha7brz867dcl9lkflck43xvz09dfmlvqyzmiz";
+      "https://github.com/NixOS/nixpkgs/archive/6d4b93323e7f78121f8d6db6c59f3889aa1dd931.tar.gz";
+    sha256 = "0g2j41cx2w2an5d9kkqvgmada7ssdxqz1zvjd7hi5vif8ag0v5la";
   };
 
 in { pkgs ? import nixpkgs { } }:
